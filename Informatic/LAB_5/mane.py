26f5479cf0f608d41740465cb17ea97615da2cb9
Kirulik <kikukatog@gmail.com>
2025-03-04 18:53:30 +0300

    add lab_5

--- Informatic/LAB_5/mane.py
+++ Informatic/LAB_5/mane.py
@@ -1,1 +1,26 @@
-print("12345")
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+df = pd.read_csv("lab_5_dop1.csv", delimiter=";")
+
+df_melted = df.melt(
+    id_vars="Дата",
+    value_vars=["Открытие", "Максимум", "Минимум", "Закрытие"],
+    var_name="Type",
+    value_name="Value"
+)
+
+plt.figure(figsize=(10, 6))
+plt.title("Статистика")
+plt.ylabel("Значение")
+plt.xlabel("Дата")
+
+sns.boxplot(
+    data=df_melted,
+    x="Дата", y="Value",
+    hue="Type", palette=["blue", "orange", "gray", "yellow"]
+)
+
+plt.savefig("task3.png")
+plt.show()
